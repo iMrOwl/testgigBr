e804eb207bf70894e09e639490e51be176e0ae66
Евгений Сычёв <sycheved@msk.bcs.ru>
2021-08-23 20:43:16 +0300

    Пробелы

--- testgigbr/SceneDelegate.swift
+++ testgigbr/SceneDelegate.swift
@@ -11,6 +11,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+
+
+
+
+
     
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
